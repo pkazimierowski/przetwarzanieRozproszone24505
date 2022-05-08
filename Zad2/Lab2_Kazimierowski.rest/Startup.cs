257623ca13c90a208d3cdeec7c31485b1ca9b1a3
pkazimierowski <patryk.kazimierowski@gmail.com>
2022-05-08 17:54:51 +0200

    added mvc service

--- Zad2/Lab2_Kazimierowski.rest/Startup.cs
+++ Zad2/Lab2_Kazimierowski.rest/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab2_Kazimierowski.rest.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,13 @@ namespace Lab2_Kazimierowski.rest
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            string connectionString = Configuration.GetConnectionString("AzureDb");
+            services.AddDbContext<AzureDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
