fb9c52cc7bcc62c11f7fed5662e7ed77897f81ec
pkazimierowski <patryk.kazimierowski@gmail.com>
2022-05-08 17:44:27 +0200

    added new data model

---
+++ Zad2/Lab2_Kazimierowski.rest/Model/Person.cs
@@ -0,0 +1,9 @@
+namespace Lab2_Kazimierowski.rest.Model
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
